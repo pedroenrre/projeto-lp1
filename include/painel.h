88ea7f15a754786b0c8c2358ec9745159c9545da
bwbruno <bruno.wagner.b.r@gmail.com>
2019-04-30 21:39:23 -0300

    Comentando interface da classe Painel

--- include/painel.h
+++ include/painel.h
@@ -9,32 +9,54 @@
 
 /// Classe Painel
 /** 
- * Responsável por gerar um painel de controle a partir de um vector
+ * Responsável por gerar um painel de controle a partir de um vector de strings
  */
 class Painel{
 	public:
-		/// Construtor de Painel.
+		/// Construtor de Painel com título e vector de opções.
 		/**
-		 * \param titulo do menu
-		 * \param vector de opções
-		 * \param deque de caminho
+		 * @param t Título do painel
+		 * @param o Vector de opções
 		*/
 		Painel(std::string t, std::vector<std::string> o);
+		/// Construtor de Painel sem caminho.
+		/**
+		 * @param t Título do painel
+		 * @param o Vector de opções
+		 * @param c Deque de caminho
+		*/
 		Painel(std::string t, std::vector<std::string> o, std::deque<std::string> c);
 		//~Painel();
 
+		
+		/// Insere o valor da opção que se deseja selecionar 
+		/** @param s seleção */
 		void setSelecao(int s);
+		
+		/// Retorna a opção selecionada atualmente 
+		/** @return Retorna um inteiro */
 		int getSelecao();
+
+		/// Verifica se a opção passada é válida
+		/**
+		 * Se o int **s** não está entre 0 (zero) e o tamanho do vector opcoes.
+		 * Um aviso de erro é setado na variável mensagem.
+		 * @param s Opção que s
+		*/
 		void verificaSelecao(int s);
 
+		/// Imprime o painel
 		void printPainel();
+
+		/// Imprime o conteúdo do deque caminho
 		void printCaminho();
 
+		/// Atribui uma frase à variável mensagem
 		void setMensagem(std::string m);
+		
+		/// Retorna o conteúdo da variável mensagem
 		std::string getMensagem();
 
-		//void setAbrir(bool a);
-		//bool getAbrir();
 
 	private:
 		std::string titulo; ///< titulo do menu
@@ -43,7 +65,6 @@ class Painel{
 		std::deque<std::string> caminho; ///< caminho de opções escolhidas até o menu atual
 		int selecao = 1; ///< opção selecionada
 		std::string mensagem;
-		//bool abrir = true;
 };
 
 #endif // __PAINEL_H__
--- src/painel.cpp
+++ src/painel.cpp
@@ -11,14 +11,14 @@ void Painel::printPainel(){
 
 	cout << "\nPET FERA "<< getSelecao() << "\n";
 	
-	printCaminho(); ///< Imprime o caminho
+	printCaminho(); // Imprime o caminho
 	
-	cout << "\n" << titulo << endl; ///< Imprime o título
+	cout << "\n" << titulo << endl; // Imprime o título
 
-	for (int i = 1; i < opcoes.size(); i++) ///< Lista as opções
+	for (int i = 1; i < opcoes.size(); i++) // Lista as opções
 		cout << "\t" << i << ". " << opcoes[i] << endl;
 
-	cout << "\n\t0. " << opcoes[0] << "\n\n"; ///< Imprime opção 0 por último
+	cout << "\n\t0. " << opcoes[0] << "\n\n"; // Imprime opção 0 por último
 	
 	if(!mensagem.empty())
 		cout << getMensagem() << endl;
@@ -47,10 +47,8 @@ void Painel::verificaSelecao(int s){
 	setSelecao(s);
 	if((s < 0) || (s > opcoes.size()-1))
 		mensagem = "Opção inválida. Tente Novamente.\n";
-	else if(s == 0)
-		caminho.pop_back();
-	//	abrir = false;
-
+	//else if(s == 0)
+	//	caminho.pop_back();
 }
 
 void Painel::setMensagem(string m){
